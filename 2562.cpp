597b000c534e56c82134006d2d9474750d498660
nopbok <nana2n@naver.com>
2024-06-26 21:51:00 +0900

    main

---
+++ 2562.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int str, arr[10], top;
+	int cnt = 1;
+	for (int i = 0; i < 9; i++) {
+		cin >> arr[i];
+	}
+	top = arr[0];
+	for (int i = 0; i < 8; i++) {
+		if (top < arr[i+1]) {
+			top = arr[i+1];
+			cnt = i + 2;
+		}
+	}
+	cout << top << '\n' << cnt;
+}
