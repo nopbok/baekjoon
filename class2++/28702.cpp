5c7ea70cada8251790ec89d9cd94feb27589aba2
myazi <nana2n@naver.com>
2024-07-13 20:55:28 +0900

    commit

--- class2++/28702.cpp
+++ class2++/28702.cpp
@@ -34,8 +34,8 @@ int main(){
     }
     else i = stoi(str3) + 1;
     if (i % 3 == 0 && i % 5 == 0) cout << "FizzBuzz" << "\n";
-    else if (i % 3 == 0 && i % 5 != 0) cout << "Fizz" << "\n";
-    else if (i % 3 != 0 && i % 5 == 0) cout << "Buzz" << "\n";
+    else if (i % 3 == 0) cout << "Fizz" << "\n";
+    else if (i % 5 == 0) cout << "Buzz" << "\n";
     else cout << i << "\n";
 
     return 0;
